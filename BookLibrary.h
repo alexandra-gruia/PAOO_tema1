996bda42318143c69d9022128bb80c8bab2e0037
alexandra-gruia <128381596+alexandra-gruia@users.noreply.github.com>
2024-11-03 22:16:03 +0200

    Update BookLibrary.h

--- BookLibrary.h
+++ BookLibrary.h
@@ -1,1 +1,15 @@
+#ifndef BOOK_LIBRARY_H
+#define BOOK_LIBRARY_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct Book {
+    string titlu;
+    string autor;
+    int id;
+};
+
+#endif
